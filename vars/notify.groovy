be7a86ec13caa74b1debc9e450ef2c3b0969ef42
Sean Flanigan <sflaniga@redhat.com>
2017-02-09 16:18:45 +1000

    Allow hipchatSend to be skipped

--- vars/notify.groovy
+++ vars/notify.groovy
@@ -1,24 +1,40 @@
 void started() {
-  hipchatSend color: "GRAY", notify: true, message: "STARTED: Job " + jobLink()
+  sendHipChat color: "GRAY", notify: true, message: "STARTED: Job " + jobLink()
 }
 
 void testResults(def testType) {
   // if tests have failed currentBuild.result will be 'UNSTABLE'
   if (currentBuild.result != null) {
-    hipchatSend color: "YELLOW", notify: true, message: "TESTS FAILED ($testType): Job " + jobLink()
+    sendHipChat color: "YELLOW", notify: true, message: "TESTS FAILED ($testType): Job " + jobLink()
   } else {
-    hipchatSend color: "GREEN", notify: true, message: "TESTS PASSED ($testType): Job " + jobLink()
+    sendHipChat color: "GREEN", notify: true, message: "TESTS PASSED ($testType): Job " + jobLink()
   }
 }
 
 void successful() {
-  hipchatSend color: "GRAY", notify: true, message: "SUCCESSFUL: Job " + jobLink()
+  sendHipChat color: "GRAY", notify: true, message: "SUCCESSFUL: Job " + jobLink()
 }
 
 void failed() {
-  hipchatSend color: "RED", notify: true, message: "FAILED: Job " + jobLink()
+  sendHipChat color: "RED", notify: true, message: "FAILED: Job " + jobLink()
 }
 
 String jobLink() {
   "<a href=\"${env.BUILD_URL}\">${env.JOB_NAME} #${env.BUILD_NUMBER}</a>"
+}
+
+private void sendHipChat(Map p) {
+  try {
+    hipchatSend(
+      color:       p.color,
+      failOnError: p.failOnError ?: false,
+      message:     p.message,
+      notify:      p.notify ?: false,
+      sendAs:      p.sendAs,
+      textFormat:  p.textFormat ?: false,
+      v2enabled:   p.v2enabled ?: false)
+  } catch (NoSuchMethodError e) {
+    println("hipchatSend skipped: no such method")
+    return
+  }
 }
