ac7b95f6dba002234f18f5d78f41cc546bc0dd43
Ding-Yi Chen <dchen@redhat.com>
2017-07-27 11:22:40 +1000

    feat(GitHubCommitStatus)

--- src/org/zanata/jenkins/Notifier.groovy
+++ src/org/zanata/jenkins/Notifier.groovy
@@ -1,12 +1,19 @@
 package org.zanata.jenkins
 
 class Notifier implements Serializable {
+  public static final String CONTEXT_UNIT = "UNIT"
+  public static final String CONTEXT_JBOSSEAP = "JBOSSEAP"
+  public static final String CONTEXT_WILDFLY8 = "WILDFLY8"
+  // Build is finished
+  public static final String CONTEXT_FINISH = "FINISH"
   private def env
   private def steps
+  private def repoUrl
 
-  Notifier(env, steps) {
+  Notifier(env, steps, repoUrl='') {
     this.env = env
     this.steps = steps
+    this.repoUrl = repoUrl
   }
 
   void started() {
@@ -21,6 +28,58 @@ class Notifier implements Serializable {
       sendHipChat color: "YELLOW", notify: true, message: "TESTS FAILED ($testType): Job " + jobLinkHtml()
     }
   }
+
+  // context: the current progress of the build, like UNIT, WILDFLY8, JBOSSEAP
+  void updateBuildStatus(String context, String result, String message = '' ){
+    switch(context){
+      case CONTEXT_UNIT:
+        if (result == 'SUCCESS'){
+          currentBuild.result = null
+        }
+        testResult(context, result);
+        break;
+      case CONTEXT_WILDFLY8:
+      case CONTEXT_JBOSSEAP:
+        currentBuild.result = result
+        testResult(context, result);
+        return;
+      case CONTEXT_FINISH:
+        if (result == 'SUCCESS' ){
+          successful();
+        }else{
+          failed();
+        }
+        break;
+      default:
+        currentBuild.result = result;
+        break;
+    }
+    setGitHubCommitStatus(context, message)
+  }
+
+  // Revised from https://issues.jenkins-ci.org/browse/JENKINS-38674
+  void setGitHubCommitStatus(String context, String message = '' ) {
+    def msg = message + ' ' + context + ': '\
+      + ((currentBuild.duration)? ' Duration: ' + currentBuild.durationString : '')\
+      + ((currentBuild.description)? ' Desc: ' + currentBuild.description: '')
+
+    step([
+      $class: 'GitHubCommitStatusSetter',
+      // Ensure it picked up from the specified URL, not zanata-pipeline-library
+      reposSource: [$class: "ManuallyEnteredRepositorySource", url: repoUrl ],
+
+      errorHandlers: [[$class: 'ShallowAnyErrorHandler']],
+      statusResultSource: [
+        $class: 'ConditionalStatusResultSource',
+        results: [
+          [$class: 'BetterThanOrEqualBuildResult', result: 'SUCCESS', state: 'SUCCESS', message: msg ],
+          [$class: 'BetterThanOrEqualBuildResult', result: 'UNSTABLE', state: 'UNSTABLE', message: msg ],
+          [$class: 'BetterThanOrEqualBuildResult', result: 'FAILURE', state: 'FAILURE', message: msg ],
+        ]
+      ]
+    ])
+  }
+
 
   void successful() {
     sendHipChat color: "GRAY", notify: true, message: "SUCCESSFUL: Job " + jobLinkHtml()
