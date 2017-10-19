deb3b8b3da46280658889bc60e341dd120216462
Ding-Yi Chen <dchen@redhat.com>
2017-10-19 18:00:20 +1000

    Use variable interpolation

--- src/org/zanata/jenkins/Reporting.groovy
+++ src/org/zanata/jenkins/Reporting.groovy
@@ -20,9 +20,9 @@ class Reporting implements Serializable {
         variable: 'CODECOV_TOKEN']]) {
         // NB the codecov script uses CODECOV_TOKEN
         // TODO use checkout.GIT_COMMIT with Jenkins 2.x (https://zanata.atlassian.net/browse/ZNTA-2237)
-        def codecovCmd = "curl -s https://codecov.io/bash | bash -s - -K -b ${env.BUILD_NUMBER} -B " + branch + " -C " + sha
+        def codecovCmd = "curl -s https://codecov.io/bash | bash -s - -K -b ${env.BUILD_NUMBER} -B $branch -C $sha"
         if (env.CHANGE_ID){
-          codecovCmd += " -P " + env.CHANGE_ID
+          codecovCmd += " -P ${env.CHANGE_ID}"
         }
 
         steps.echo codecovCmd
