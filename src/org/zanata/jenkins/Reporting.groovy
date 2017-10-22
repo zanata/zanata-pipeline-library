dd51ef7b5f13f235a1410ded7993402793c83656
Ding-Yi Chen <dchen@redhat.com>
2017-10-22 23:07:28 +1000

    Get credentialId from repoUrl

--- src/org/zanata/jenkins/Reporting.groovy
+++ src/org/zanata/jenkins/Reporting.groovy
@@ -8,15 +8,16 @@ class Reporting implements Serializable {
 
   // Send test coverage data to codecov.io
   @NonCPS
-  static void codecov(def env, def steps, String repoUrl){
+  static void codecov(def env, def steps, String repoUrl, String credentialId = null){
     ScmGit scmGit = new ScmGit(env, steps, repoUrl)
     String sha = scmGit.getCommitId(env.BRANCH_NAME)
     String branch = scmGit.getBranch(sha)
+    credentialId = credentialId ?: 'codecov_' + repoUrl.replace(/^.*\//, '')
 
     try {
       withCredentials(
         [[$class: 'StringBinding',
-        credentialsId: 'codecov_zanata-platform',
+        credentialsId: credentialId,
         variable: 'CODECOV_TOKEN']]) {
         // NB the codecov script uses CODECOV_TOKEN
         // TODO use checkout.GIT_COMMIT with Jenkins 2.x (https://zanata.atlassian.net/browse/ZNTA-2237)
